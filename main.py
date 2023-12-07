7458a1ee75a06ca6eca65fcc2e9d54ccfe766f29
Raja Chowdhury <110104095+RCgit123@users.noreply.github.com>
2023-12-07 11:21:08 +0000

    prediction file added

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from IOT_NIDS.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipel
 from IOT_NIDS.pipeline.stage_02_model_evaluation import ModelEvaluationPipeline
 
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 # solved huge file upload error: https://github.blog/2017-06-27-git-lfs-2-2-0-released/
 STAGE_NAME = "Model Evaluation stage"
